bc40974427b62c0386b9eea4131e9cd397cb2ac1
SIYEENOVE <mosiwi@outlook.com>
2025-10-28 11:08:13 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -4,99 +4,67 @@
 mShield.irCallBack(function () {
     // Judge the received key value
     if (mShield.irButton(mShield.MshieldIrButtons.Number1)) {
-
         // The four LED lights of the expansion board are on for 1 second.
         mShield.setLed(mShield.Leds.AllLED, true)
         basic.pause(1000)
     }
     if (mShield.irButton(mShield.MshieldIrButtons.Number2)) {
-
         // The four LED lights of the expansion board are off for 1 second.
         mShield.setLed(mShield.Leds.AllLED, false)
         basic.pause(1000)
     }
 })
-
 // The speed compensation value of the 2 motors is set to 0.
 mShield.motorsAdjustment(0, 0)
-
 basic.forever(function () {
     // 2 motors are running forward for 1 second.
     mShield.setMotorsDirectionSpeed(mShield.Motors.AllMotors, mShield.MotorsDirection.CC, 100)
     basic.pause(1000)
-
     // The 2 motors are stopped for 1 second.
     mShield.setMotorsDirectionSpeed(mShield.Motors.AllMotors, mShield.MotorsDirection.CC, 0)
     basic.pause(1000)
-
     // The 2 motors are reversed for 1 second.
     mShield.setMotorsDirectionSpeed(mShield.Motors.AllMotors, mShield.MotorsDirection.CCW, 100)
     basic.pause(1000)
-
     // The 2 motors are stopped for 1 second.
     mShield.setMotorsDirectionSpeed(mShield.Motors.AllMotors, mShield.MotorsDirection.CCW, 0)
     basic.pause(1000)
-
     // Set the S1--S4 interface of the expansion board to PWM mode.
     mShield.setS1ToS4Type(mShield.S1ToS4Type.PWM)
     basic.pause(1000)
-
     // S1--S4 PWM interface output 200 value, 100% duty cycle.
-    mShield.extendPwmControl(mShield.PwmAndServoIndex.S1, 200)
-    mShield.extendPwmControl(mShield.PwmAndServoIndex.S2, 200)
-    mShield.extendPwmControl(mShield.PwmAndServoIndex.S3, 200)
-    mShield.extendPwmControl(mShield.PwmAndServoIndex.S4, 200)
+    mShield.extendPwmControl(mShield.PwmAndServoIndex.All, 200)
     basic.pause(1000)
-
     // S1--S4 PWM interface output 0 value, 0% duty cycle.
-    mShield.extendPwmControl(mShield.PwmAndServoIndex.S1, 0)
-    mShield.extendPwmControl(mShield.PwmAndServoIndex.S2, 0)
-    mShield.extendPwmControl(mShield.PwmAndServoIndex.S3, 0)
-    mShield.extendPwmControl(mShield.PwmAndServoIndex.S4, 0)
+    mShield.extendPwmControl(mShield.PwmAndServoIndex.All, 0)
     basic.pause(1000)
-
     // Set the S1--S4 interface of the expansion board to drive servo mode.
     mShield.setS1ToS4Type(mShield.S1ToS4Type.Servo)
     basic.pause(1000)
-
     // The servo of S1--S4 interface is turned to 180 degrees.
-    mShield.extendServoControl(mShield.PwmAndServoIndex.S1, mShield.ServoType.Servo180, 0)
-    mShield.extendServoControl(mShield.PwmAndServoIndex.S2, mShield.ServoType.Servo180, 0)
-    mShield.extendServoControl(mShield.PwmAndServoIndex.S3, mShield.ServoType.Servo180, 0)
-    mShield.extendServoControl(mShield.PwmAndServoIndex.S4, mShield.ServoType.Servo180, 0)
+    mShield.extendServoControl(mShield.PwmAndServoIndex.All, mShield.ServoType.Servo180, 0)
     basic.pause(1000)
-
     // The servo of S1--S4 interface is turned to 0 degrees.
-    mShield.extendServoControl(mShield.PwmAndServoIndex.S1, mShield.ServoType.Servo180, 180)
-    mShield.extendServoControl(mShield.PwmAndServoIndex.S2, mShield.ServoType.Servo180, 180)
-    mShield.extendServoControl(mShield.PwmAndServoIndex.S3, mShield.ServoType.Servo180, 180)
-    mShield.extendServoControl(mShield.PwmAndServoIndex.S4, mShield.ServoType.Servo180, 180)
+    mShield.extendServoControl(mShield.PwmAndServoIndex.All, mShield.ServoType.Servo180, 180)
     basic.pause(1000)
-
     // Set the S1--S4 interface of the expansion board to drive servo mode.
     mShield.setS1ToS4Type(mShield.S1ToS4Type.Servo)
     basic.pause(1000)
-
     // 360-degree servo maximum speed forward rotation.
-    mShield.continuousServoControl(mShield.PwmAndServoIndex.S1, 100)
+    mShield.continuousServoControl(mShield.PwmAndServoIndex.All, 100)
     basic.pause(1000)
-
     // 360-degree servo stop.
-    mShield.continuousServoControl(mShield.PwmAndServoIndex.S1, 0)
+    mShield.continuousServoControl(mShield.PwmAndServoIndex.All, 0)
     basic.pause(1000)
-
     // 360-degree servo maximum speed reversal.
-    mShield.continuousServoControl(mShield.PwmAndServoIndex.S1, -100)
+    mShield.continuousServoControl(mShield.PwmAndServoIndex.All, -100)
     basic.pause(1000)
-
     // 360-degree servo stop.
-    mShield.continuousServoControl(mShield.PwmAndServoIndex.S1, 0)
+    mShield.continuousServoControl(mShield.PwmAndServoIndex.All, 0)
     basic.pause(1000)
-
     // Read the power level of the 3-connected AA battery.
     basic.showNumber(mShield.batteryLevel(mShield.BatteryType.AA3))
     basic.pause(1000)
-
     // Read the extension board firmware version.
     basic.showString(mShield.readVersions())
     basic.pause(1000)
